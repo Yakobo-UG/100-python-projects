dfb81366ff9ee334a9037dba9c6bacbb86ef0340
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-12 13:30:04 +0300

    madlibs
    added another print method

--- madlibs/madlibs game 1.py
+++ madlibs/madlibs game 1.py
@@ -3,3 +3,6 @@ verb = str(input("Enter verb here: "))
 verb2 = str(input("Enter verb here: "))
 madlibs = f" hacking is on of {verb} best decision {adj} one could ever {verb2} in his or her life"
 print(madlibs)
+#with a diferrent form of concatnation
+madlibs = " hacking is on of" + verb +  " best decision" + adj + "one could ever" + verb2 +  "in his or her life"
+print(madlibs)
