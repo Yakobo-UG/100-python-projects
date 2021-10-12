39d9d4b54e3dfe7242b72707db43413202585588
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-12 10:18:10 +0300

    madlibs
    simple madlibs game

---
+++ madlibs/madlibs game 1.py
@@ -0,0 +1,5 @@
+adj = str(input("Enter adj here: "))
+verb = str(input("Enter verb here: "))
+verb2 = str(input("Enter verb here: "))
+madlibs = f" hacking is on of {verb} best decision {adj} one could ever {verb2} in his or her life"
+print(madlibs)
